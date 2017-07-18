f257fa596e3034871598815c3bdf28742718b62a
sofish <happinesz@gmail.com>
2017-07-18 18:26:40 +0800

    code format

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const DB_SETTING_KEY = 'nodebb-plugin-qiniu-file';
 const DB = module.parent.require('./database');
 const qiniu = require('qiniu');
 
-(function(E) {
+(function (E) {
 
-	E.init = function(params, callback) {
+  E.init = function (params, callback) {
 
-		params.router.get('/admin/plugins/qiniu', params.middleware.applyCSRF, params.middleware.admin.buildHeader, renderAdmin);
-		params.router.get('/api/admin/plugins/qiniu', params.middleware.applyCSRF, renderAdmin);
-		params.router.post('/api/admin/plugins/qiniu/save', params.middleware.applyCSRF, saveSettings);
+    params.router.get('/admin/plugins/qiniu', params.middleware.applyCSRF, params.middleware.admin.buildHeader, renderAdmin);
+    params.router.get('/api/admin/plugins/qiniu', params.middleware.applyCSRF, renderAdmin);
+    params.router.post('/api/admin/plugins/qiniu/save', params.middleware.applyCSRF, saveSettings);
 
-		callback();
-	};
+    callback();
+  };
 
   E.upload = function (data, callback) {
 
-		new Promise(function(r, rj) {
+    new Promise(function (r, rj) {
       DB.getObject(DB_SETTING_KEY, function (err, res) {
-				if(err) return rj(err);
-				r(res);
+        if (err) return rj(err);
+        r(res);
       })
-		}).then(function(settings) {
-			if(!settings || !settings.key || !settings.secret || !settings.bucket || !settings.domain) return callback(new Error('invaild key, secret or bucket'));
-			doUpload(data, settings, callback);
+    }).then(function (settings) {
+      if (!settings || !settings.key || !settings.secret || !settings.bucket || !settings.domain) return callback(new Error('invaild key, secret or bucket'));
+      doUpload(data, settings, callback);
     }).catch(function (err) {
       callback(new Error(err));
-    }) ;
+    });
 
   };
 
   var admin = {};
 
-  admin.menu = function(menu, callback) {
+  admin.menu = function (menu, callback) {
     menu.plugins.push({
       route: '/plugins/qiniu',
       icon: 'fa-cloud-upload',
@@ -48,35 +48,37 @@ const qiniu = require('qiniu');
 
   E.admin = admin;
 
-	function renderAdmin(req, res, next) {
-    DB.getObject(DB_SETTING_KEY, function(err, settings) {
-			if (err) return next(err);
+  function renderAdmin(req, res, next) {
+    DB.getObject(DB_SETTING_KEY, function (err, settings) {
+      if (err) return next(err);
 
-			settings = settings || {};
-			res.render('admin/plugins/qiniu', {settings: {
-        key: settings.key,
-				secret: settings.secret,
-        bucket: settings.bucket,
-        domain: settings.domain
-      }, csrf: req.csrfToken()});
-		});
-	}
+      settings = settings || {};
+      res.render('admin/plugins/qiniu', {
+        settings: {
+          key: settings.key,
+          secret: settings.secret,
+          bucket: settings.bucket,
+          domain: settings.domain
+        }, csrf: req.csrfToken()
+      });
+    });
+  }
 
-	function saveSettings(req, res, next) {
-		var data = {
+  function saveSettings(req, res, next) {
+    var data = {
       key: req.body.key || '',
       secret: req.body.secret || '',
-			bucket: req.body.bucket || '',
+      bucket: req.body.bucket || '',
       domain: req.body.domain || ''
-		};
+    };
 
-    DB.setObject(DB_SETTING_KEY, data, function(err) {
-			if (err) return next(err)
-			res.status(200).json({message: 'Settings saved!'});
-		});
-	}
+    DB.setObject(DB_SETTING_KEY, data, function (err) {
+      if (err) return next(err)
+      res.status(200).json({message: 'Settings saved!'});
+    });
+  }
 
-	function doUpload(data, settings, callback) {
+  function doUpload(data, settings, callback) {
 
     var file = data.image || data.file;
     var type = file.url ? 'url' : 'file';
@@ -89,7 +91,7 @@ const qiniu = require('qiniu');
     var config = new qiniu.conf.Config();
 
     if (type === 'file') {
-      var uploadToken = new qiniu.rs.PutPolicy({ scope: settings.bucket }).uploadToken(mac);
+      var uploadToken = new qiniu.rs.PutPolicy({scope: settings.bucket}).uploadToken(mac);
       var putExtra = new qiniu.form_up.PutExtra();
       var formUploader = new qiniu.form_up.FormUploader(config);
 
@@ -102,18 +104,18 @@ const qiniu = require('qiniu');
     }
 
     function response(err, body, info) {
-			if (err) return callback(err);
+      if (err) return callback(err);
 
-			if (info.statusCode == 200) {
-				return callback(null, {
-					url: settings.domain.replace(/\/$/, '') + '/' + body.hash,
-					name: body.key
-				});
-			}
+      if (info.statusCode == 200) {
+        return callback(null, {
+          url: settings.domain.replace(/\/$/, '') + '/' + body.hash,
+          name: body.key
+        });
+      }
 
-			callback(new Error('unknown error occurs while uploading'));
+      callback(new Error('unknown error occurs while uploading'));
     }
 
-	}
+  }
 }(module.exports));
 
